8300d4622417633642130660e4b7feb8b7f6a343
moti levi <mordilevi11@gmail.com>
2020-12-10 16:32:31 +0200

    5 commit

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+app.get('/test', (req,res)=>{
+    res.send("works")
+})
 app.listen(port, () => {
    console.log('Server is up!');
 });
