99ae108a9cdfd189b09b57253a0d525648cb60e2
moti levi <mordilevi11@gmail.com>
2020-12-10 16:48:19 +0200

    6 commit

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+
 app.get('/', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/test', (req,res)=>{
--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hallo  moti</h1>
+      <h1>hallo moti</h1>
+      
     </div>
   );
 }
