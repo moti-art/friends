b7375bbf74909927da52d0a99c398648aff06894
moti levi <mordilevi11@gmail.com>
2020-12-14 13:47:50 +0200

    2222

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import React, { useState , useEffect} from 'react';
+// import React, { useState , useEffect} from 'react';
 
 
 export default function App() {
 
 
-  const [details, setDetails] = useState([]);
+  // const [details, setDetails] = useState([]);
 
-    useEffect(() => {
-        async function blah() {
-            let data = await fetch('https://api.themoviedb.org/3/movie/550?api_key=8a4a67f63953a2afa86c2fb2a94f6f1e')
-            let res = await data.json()
-            setDetails(res)
-            // console.log(details.genres[0].name);
-        }
-        blah()
-    }, [])
+  //   useEffect(() => {
+  //       async function blah() {
+  //           let data = await fetch('https://api.themoviedb.org/3/movie/550?api_key=8a4a67f63953a2afa86c2fb2a94f6f1e')
+  //           let res = await data.json()
+  //           setDetails(res)
+  //           // console.log(details.genres[0].name);
+  //       }
+  //       blah()
+  //   }, [])
 
 
   return (
