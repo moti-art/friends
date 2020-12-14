64b749f97ce97874a333024f07f1d80de464e3f4
moti levi <mordilevi11@gmail.com>
2020-12-14 10:24:19 +0200

    14.12 commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'build');
+// const axios = require('axios')
+const publicPath = path.join(__dirname, "../build");
 const port = process.env.PORT || 3000;
-
+console.log(publicPath)
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-   res.sendFile(path.join(publicPath, 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "/index.html"));
 });
 
-app.get('/test', (req,res)=>{
-    res.send("works babe")
-})
+app.get("/test", (req, res) => {
+  res.send("haha");
+});
+
+// axios.get('https://api.themoviedb.org/3/movie/550?api_key=8a4a67f63953a2afa86c2fb2a94f6f1e')
+// .then(response => {
+//   console.log(response.data);
+// })
+// .catch(error => {
+//   console.log(error);
+// });
+
 app.listen(port, () => {
-   console.log('Server is up!');
+  console.log("Server is up!");
 });
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios'
 
-function App() {
+
+export default function App() {
+ 
   return (
     <div className="App">
-      <h1>hallo moti</h1>
-      
+      <h1>hallo version 2 !!!</h1>
+      <h2>boom</h2>
     </div>
   );
 }
 
-export default App;
+
